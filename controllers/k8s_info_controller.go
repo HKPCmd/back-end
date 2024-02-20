7db56c9f7cfde6ebdc3f3ea16c38f56a40261288
dusdjhyeon <dusdj0813@gmail.com>
2024-02-20 16:45:54 +0900

    feat: get kubeconfig and client info

---
+++ controllers/k8s_info_controller.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"os"
+	"fmt"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func getKubeConfig() (kubernetes.Interface){
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("error getting user home dir: %v\n", err)
+		os.Exit(1)
+	}
+	kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
+	fmt.Printf("Using kubeconfig: %s\n", kubeConfigPath)
+
+	kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+	if err != nil {
+		fmt.Printf("Error getting kubernetes config: %v\n", err)
+		os.Exit(1)
+	}
+
+	clientset, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		fmt.Printf("error getting kubernetes config: %v\n", err)
+		os.Exit(1)
+	}
+
+	return clientset
+}
