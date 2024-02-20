21f81ef0a046c77a83b7b4382ad637bb2f0d6aba
dusdjhyeon <dusdj0813@gmail.com>
2024-02-20 16:45:40 +0900

    feat: set main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"main/controllers"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/namespaces", controllers.GetNamespaces)
+	r.GET("/pods", controllers.GetPods)
+
+	if err := r.Run(":8000"); err != nil {
+		fmt.Printf("Error to connect 8000")
+	}
+}
