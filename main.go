e54f93370f8d952561528bfee9b4effb228de70a
dusdjhyeon <dusdj0813@gmail.com>
2024-02-23 16:55:10 +0900

    feat: set socket-io conn

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	socketio "github.com/googollee/go-socket.io"
 	"main/controllers"
 )
 
@@ -18,7 +19,20 @@ func main() {
 	r.GET("/context", controllers.GetCurrentContext)
 	r.GET("/namespaces", controllers.GetNamespaces)
 	r.GET("/pods", controllers.GetPods)
-	r.GET("/ws", controllers.HandleConnections)
+	
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	controllers.HandleConnections(server)
+
+	r.GET("/ws", func(c *gin.Context) {
+		server.ServeHTTP(c.Writer, c.Request)
+	})
+
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Not found"})
+	})
 
 	if err := r.Run(":8000"); err != nil {
 		fmt.Printf("Error to connect 8000")
