b50d2099a75db58eb07d37fe3b87d55c5fbf7887
dusdjhyeon <dusdj0813@gmail.com>
2024-02-28 16:43:09 +0900

    feat: retrying connection

--- controllers/websocket_connect_controller.go
+++ controllers/websocket_connect_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"log"
+	"time"
 	"net/http"
 
 	"main/models"
@@ -26,11 +27,45 @@ func HandleConnections(c *gin.Context) {
 	}
 	defer ws.Close()
 
+	go func() {
+		var retryInterval = time.Second
+		var maxRetryInterval = time.Minute * 5
+		var retries = 0
+
+		for {
+			if err := ws.WriteMessage(websocket.PingMessage, nil); err != nil {
+				log.Printf("websocket connection lost: %v", err)
+
+				for {
+					ws, _, err = websocket.DefaultDialer.Dial("ws://172.25.219.226:8000/ws", nil)
+					if err != nil {
+						log.Printf("error retrying connection: %v", err)
+						retries++
+						if retries > 10 {
+							log.Printf("max retries exceeded")
+							return 
+						}
+						time.Sleep(retryInterval)
+						if retryInterval < maxRetryInterval {
+							retryInterval *= 2
+						}
+					} else {
+						retryInterval = time.Second
+						retries = 0
+						break
+					}
+				}
+			}
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
 	for {
 		var msg models.Message
 		err := ws.ReadJSON(&msg)
 		if err != nil {
 			log.Printf("error: %v", err)
+			
 			break
 		}
 
